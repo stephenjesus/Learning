730e2c897277a4860eff1b669a49635c5f91a46b
Stephen Prakash <stephen@examly.in>
2019-03-23 18:02:23 +0530

    temp change

--- src/app/aptitude/aptitude.component.ts
+++ src/app/aptitude/aptitude.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AptitudeComponent implements OnInit {
   collection = [];
+  currentQ;
+  toggle = false;
   constructor() {
+    const json = {
+      q_data : 'A person crosses a 600 m long street in 5 minutes. What is his speed in km per hour?',
+      answer : '7.2' ,
+      explanation :  'Speed =		600	m/sec. 5 x 60 = 2 m/sec.' ,
+      options: [
+        3.6,
+        7.2,
+        8.4,
+        10
+      ]
+    };
     for (let i = 1; i <= 100; i++) {
-      this.collection.push(`item ${i}`);
+      this.collection.push(json);
     }
+   }
+   viewanswer(item) {
+    this.toggle = !this.toggle;
+    if (this.toggle) {
+      this.currentQ = item;
+    } else {
+      this.currentQ = [];
+    }
+
    }
 
   ngOnInit() {
